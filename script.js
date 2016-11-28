d68b9cd9ab5172bad1a2040f9236db975cb4d08c
Sherrie Bloomquist <sherrie.bloomquist@yahoo.com>
2016-11-27 19:06:06 -0600

    basic functionality working

--- script.js
+++ script.js
@@ -4,14 +4,22 @@ var employee = [];
 
 //enter new employee data
 var addEmployee = function (firstName, lastName, idNumber, jobTitle, annualSalary){
- var newEmp = {
-   first: document.getElementById( 'firstName' ).value,
-   last: document.getElementById( 'lastName' ).value,
-   id: document.getElementById( 'idNumber' ).value,
-   job: document.getElementById( 'jobTitle' ).value,
-   salary: document.getElementById( 'annualSalary' ).value,
-};
+   var firstN = document.getElementById( 'firstName' ).value;
+   var lastN = document.getElementById( 'lastName' ).value;
+   var idNum = document.getElementById( 'idNumber' ).value;
+   var job = document.getElementById( 'jobTitle' ).value;
+   var annSalary = document.getElementById( 'annualSalary' ).value;
+   document.getElementById("monthlySalaryTotal").innerHTML=monthlySalaryTotal();
+
+
+var newEmp = [firstN, lastN, idNum, job, annSalary];
 employee.push(newEmp);
+console.log(newEmp);
+
+//add employee info to DOM
+document.getElementById('listOfEmployees').innerHTML+=("<tr><td>"+ firstN +"</td><td>" + lastN + "</td><td>"+ idNum + "</td><td>" + job + "</td><td>" +
+annSalary +"</tr></td>");
+globalSalary =+ newEmp.annualSalary;
 };// end enter new employee data
 
 //clearFields upon mouseup
@@ -22,3 +30,12 @@ var clearFields = function() {
      document.getElementById( 'jobTitle' ).value = "",
      document.getElementById( 'annualSalary' ).value = ""
 };//end clearFields
+
+//calculate monthlySalaryTotal
+var monthlySalaryTotal = function(){
+  var globalSalary = 0;
+  for(i=0; i<employee.length; i++){
+  globalSalary += Number(employee[i][4]);
+}
+  return globalSalary/12;
+};
